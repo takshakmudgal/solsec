3effba5b4f241200658c4332f93e3c1e5c0436fb
takshakmudgal <takshakmudgal@gmail.com>
2025-04-29 23:00:24 +0530

    init: solsec dashboard

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { HydrateClient } from "~/trpc/server";
+import Image from "next/image";
+
+export default async function Home() {
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen items-center justify-center bg-[radial-gradient(circle,_rgba(0,0,0,1)_0%,_rgba(36,58,89,1)_73%,_rgba(59,25,25,1)_100%)] text-white">
+        <div className="text-center">
+          <h1 className="text-4xl text-white">
+            <span className="title-gradient">solsec</span> dashboard
+          </h1>
+          <p className="text-lg leading-none text-gray-300">
+            The best comprehensive open-source tracker for <br />
+            <span className="flex items-center justify-center">
+              <Image src="solanaLogo.svg" alt="solana" width={80} height={80} />
+              &nbsp; ecosystem security exploits
+            </span>
+          </p>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
